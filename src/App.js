1f879370e8603cae39c98416bd180bf770aefddc
Jay Shah <jayshah.9596@gmail.com>
2018-09-12 19:02:42 +0530

    App changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import {Provider} from 'react-redux';
 
-import { connect } from 'react-redux';
-import { defaultFunction } from './actions';
-import SimpleComponent from './components/simple-component';
+// import logo from "./logo.svg";
+import "./App.css";
+
+import { connect } from "react-redux";
+import { defaultFunction } from "./actions";
+import store from './store';
+
+import SignupForm from "./components/sign-up-form";
+import LoginForm from "./components/login-form";
+import SimpleComponent from "./components/simple-component";
 
 class App extends Component {
-
   componentDidMount() {
     // call default function to display redux operation
-    this.props.defaultFunction();
+    // this.props.defaultFunction();
   }
 
   render() {
     return (
-      <div>
-        React Redux Starter Template
-        <SimpleComponent/>
-      </div>
+      <Provider store={store}>
+      <Router className="App">
+        <div>
+          <h1 className='App-title'>Title Template</h1>
+          <div className="navbar">
+            <ul>
+            <li>
+                <Link to="/" className="active">HOME</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign-up</Link>
+              </li>
+            </ul>
+            <hr />
+          </div>
+          <Switch>
+          <Route exact path="/" component={SimpleComponent} />
+          <Route path="/signup" component={SignupForm} />
+          <Route path="/login" component={LoginForm} />
+          </Switch>
+        </div>
+      </Router>
+      </Provider>
     );
   }
 }
@@ -30,4 +58,7 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { defaultFunction })(App);
+export default connect(
+  mapStateToProps,
+  { defaultFunction }
+)(App);
