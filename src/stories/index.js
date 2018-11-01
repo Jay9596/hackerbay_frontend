bae390b2522ea633152643c2e7e9349a52e10488
Jay Shah <Jay9596@users.noreply.github.com>
2018-11-01 12:10:20 +0530

    Website stories - babel config update

--- src/stories/index.js
+++ src/stories/index.js
@@ -7,6 +7,9 @@ import store from "../store";
 import SimpleComponent from "../components/simple-component";
 import Login from "../components/login-form";
 import Signup from "../components/sign-up-form";
+import AddWebsite from "../components/website_add";
+import ListWebsite from "../components/website_list";
+import WebsiteInfo from "../components/website_info";
 
 storiesOf("Simple Component", module).add("Simple Component", () => (
   <SimpleComponent />
@@ -19,3 +22,15 @@ storiesOf("Login Form", module)
 storiesOf("Signup Form", module)
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
   .add("Signup Form", () => <Signup />);
+
+storiesOf("Add Website Component", module)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
+  .add("Add Website", () => <AddWebsite />);
+
+storiesOf("List Website Component", module).add("Website Info", () => (
+  <WebsiteInfo
+    name="Name of Website"
+    url="www.url_of_website.com"
+    status="online"
+  />
+));
